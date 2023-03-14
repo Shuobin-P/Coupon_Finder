0df1f51ce6164195c10d9433d31c704d4cb2d8f5
Shuobin-P <3120465015@qq.com>
2023-03-14 18:29:27 +0800

    add a function: get User's Available Coupons

---
+++ src/main/java/com/google/couponfinder/dto/WalletCouponDTO.java
@@ -0,0 +1,16 @@
+package com.google.couponfinder.dto;
+
+import lombok.Data;
+
+/**
+ * @author W&F
+ * @version 1.0
+ * @date 2023/3/14 17:41
+ */
+@Data
+public class WalletCouponDTO {
+    private String title;
+    private String description;
+    private String pictureUrl;
+
+}
