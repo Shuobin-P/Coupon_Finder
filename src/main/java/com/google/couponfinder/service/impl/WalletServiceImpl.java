eb32c2419302e9847ceecd7cbdf43af05b90ceab
Shuobin-P <3120465015@qq.com>
2023-03-19 17:40:44 +0800

    add new function: customer show coupon qrCode

--- src/main/java/com/google/couponfinder/service/impl/WalletServiceImpl.java
+++ src/main/java/com/google/couponfinder/service/impl/WalletServiceImpl.java
@@ -3,6 +3,7 @@ package com.google.couponfinder.service.impl;
 import com.github.pagehelper.Page;
 import com.google.couponfinder.dto.WalletCouponDTO;
 import com.google.couponfinder.mapper.CouponMapper;
+import com.google.couponfinder.mapper.UserMapper;
 import com.google.couponfinder.service.WalletService;
 import com.google.couponfinder.util.TokenUtils;
 import com.google.couponfinder.vo.ResultVO;
@@ -18,11 +19,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class WalletServiceImpl implements WalletService {
     private final TokenUtils tokenUtils;
+    private final UserMapper userMapper;
     private final CouponMapper couponMapper;
 
     @Autowired
-    public WalletServiceImpl(TokenUtils tokenUtils, CouponMapper couponMapper) {
+    public WalletServiceImpl(TokenUtils tokenUtils, UserMapper userMapper, CouponMapper couponMapper) {
         this.tokenUtils = tokenUtils;
+        this.userMapper = userMapper;
         this.couponMapper = couponMapper;
     }
 
@@ -44,5 +47,15 @@ public class WalletServiceImpl implements WalletService {
         couponMapper.deleteCoupon(open_id, id);
         couponMapper.minusCouponCollectedQuantity(id);
         return ResultVO.getInstance("成功删除优惠券", null);
+    }
+
+    @Override
+    public ResultVO getWalletID(String jwt) {
+        if (jwt == null) {
+            return ResultVO.getInstance(400, "请先完成身份认证", null);
+        }
+        String open_id = tokenUtils.getUsernameByToken(jwt);
+        Long walletID = userMapper.getCardPackageID(open_id);
+        return ResultVO.getInstance("成功查询到用户对于的卡包ID", walletID);
     }
 }
