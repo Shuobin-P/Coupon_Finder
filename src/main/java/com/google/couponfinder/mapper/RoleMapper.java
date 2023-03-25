46399131871a60ae2ec0d01cdef1837b30db6242
Shuobin-P <3120465015@qq.com>
2023-03-25 18:01:53 +0800

    Fix bug: fix some bug of Merchant Identity

--- src/main/java/com/google/couponfinder/mapper/RoleMapper.java
+++ src/main/java/com/google/couponfinder/mapper/RoleMapper.java
@@ -5,11 +5,9 @@ import org.apache.ibatis.annotations.Mapper;
 /**
  * @author W&F
  * @version 1.0
- * @date 2023/3/15 21:23
+ * @date 2023/3/25 15:40
  */
 @Mapper
-public interface UserMapper {
-    Long getCardPackageID(String open_id);
-
-    Long getUserID(String openID);
+public interface RoleMapper {
+    Long getRoleID(String role);
 }
--- src/main/java/com/google/couponfinder/service/impl/MerchantServiceImpl.java
+++ src/main/java/com/google/couponfinder/service/impl/MerchantServiceImpl.java
@@ -1,5 +1,8 @@
 package com.google.couponfinder.service.impl;
 
+import com.google.couponfinder.mapper.RoleMapper;
+import com.google.couponfinder.mapper.UserMapper;
+import com.google.couponfinder.mapper.UserRoleMapper;
 import com.google.couponfinder.service.MerchantService;
 import com.google.couponfinder.util.TokenUtils;
 import com.google.couponfinder.vo.ResultVO;
@@ -7,6 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author W&F
@@ -20,10 +26,16 @@ public class MerchantServiceImpl implements MerchantService {
     private String secret;
 
     private final TokenUtils tokenUtils;
+    private final UserMapper userMapper;
+    private final RoleMapper roleMapper;
+    private final UserRoleMapper userRoleMapper;
 
     @Autowired
-    public MerchantServiceImpl(TokenUtils tokenUtils) {
+    public MerchantServiceImpl(TokenUtils tokenUtils, UserMapper userMapper, RoleMapper roleMapper, UserRoleMapper userRoleMapper) {
         this.tokenUtils = tokenUtils;
+        this.userMapper = userMapper;
+        this.roleMapper = roleMapper;
+        this.userRoleMapper = userRoleMapper;
     }
 
     @Override
@@ -39,6 +51,12 @@ public class MerchantServiceImpl implements MerchantService {
             token = tokenUtils.refreshToken(token);
         }
         token = tokenUtils.addClaim(token, "isMerchant", true);
-        return ResultVO.getInstance(1, "成功添加商家权限", token);
+        //FIXME 一旦完成身份认证，数据库中应该就要保存用户的商家身份信息，下次登录的时候，用户就不要进行身份验证了。
+        Long userID = userMapper.getUserID(tokenUtils.getUsernameByToken(Authorization));
+        Long roleID = roleMapper.getRoleID("merchant");
+        userRoleMapper.insert(userID,roleID);
+        Map map = new HashMap(1);
+        map.put("token", token);
+        return ResultVO.getInstance(1, "成功添加商家权限", map);
     }
 }
